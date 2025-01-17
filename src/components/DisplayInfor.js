b35230ce83f9945d83c1c7901fba1144145fb3ed
vandat3012 <luongvandat301220@gmail.com>
2025-01-17 16:03:39 +0700

    Conditional output

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,22 +1,44 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+    state = {
+        isShowList: true
+    }
+
+    hanldShowHide = () => {
+        this.setState({
+            isShowList: !this.state.isShowList
+        })
+    }
 
     render() {
         // dùng destructuring
         const { listUser } = this.props;
+        console.log(listUser)
         return (
-            // props propertis <dùng để truyền dữ liệu từ cha sang con>
             <div>
-                {listUser.map((user) => {
-                    return (
-                        <div key={user.id}>
-                            <div>My name is Dat {user.name}</div>
-                            <div>My age is 23 {user.age}</div>
-                            <hr />
-                        </div>
-                    )
-                })}
+                <div>
+                    <span onClick={() => { this.hanldShowHide() }}> {
+                        this.state.isShowList === true ? "Hide list" : "On List"}
+                    </span>
+                </div>
+                {/* // props propertis <dùng để truyền dữ liệu từ cha sang con> */}
+                {this.state.isShowList &&
+                    <div>
+                        {
+                            listUser.map((user, index) => {
+                                return (
+                                    <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                        <div>My name is Dat {user.name}</div>
+                                        <div>My age is 23 {user.age}</div>
+                                        <hr />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                }
+
             </div>
         )
     }
--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -6,9 +6,9 @@ class Mycomponent extends React.Component {
 
     state = {
         listUser: [
-            { id: 1, name: "đat1", age: "26" },
+            { id: 1, name: "đat1", age: "16" },
             { id: 2, name: "đat2", age: "27" },
-            { id: 3, name: "đat3", age: "26" },
+            { id: 3, name: "đat3", age: "30" },
         ]
     }
     render() {
